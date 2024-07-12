b8a186c11eb771a11374b934d0bcc77a886b02c1
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-12 10:14:31 -0300

    feat: finis check personal loan avaliability

--- src/main/java/com/gabrielvicente/loans/dto/CustomerLoanRequest.java
+++ src/main/java/com/gabrielvicente/loans/dto/CustomerLoanRequest.java
@@ -1,8 +1,14 @@
 package com.gabrielvicente.loans.dto;
 
+import com.gabrielvicente.loans.domain.Customer;
+
 public record CustomerLoanRequest(Integer age,
                                   String cpf,
                                   String name,
                                   Double income,
                                   String location) {
+
+    public Customer toDomain() {
+        return new Customer(age, cpf, name, income, location);
+    }
 }
--- src/main/java/com/gabrielvicente/loans/service/LoanService.java
+++ src/main/java/com/gabrielvicente/loans/service/LoanService.java
@@ -1,14 +1,29 @@
 package com.gabrielvicente.loans.service;
 
+import com.gabrielvicente.loans.domain.Customer;
+import com.gabrielvicente.loans.domain.Loan;
+import com.gabrielvicente.loans.domain.LoanType;
 import com.gabrielvicente.loans.dto.CustomerLoanRequest;
 import com.gabrielvicente.loans.dto.CustomerLoanResponse;
+import com.gabrielvicente.loans.dto.LoanResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LoanService {
 
     public CustomerLoanResponse checkPossibleLoans(CustomerLoanRequest customerLoanRequest) {
-        return null;
+        Customer customer = customerLoanRequest.toDomain();
+        Loan loan = new Loan(customer);
 
+        List<LoanResponse> loans = new ArrayList<>();
+
+        if (loan.isPersonalLoanAvailable()) {
+            loans.add(new LoanResponse(LoanType.PERSONAL, loan.getPersonalLoanInterestRate()));
+        }
+
+        return new CustomerLoanResponse(customerLoanRequest.name(), loans);
     }
 }
