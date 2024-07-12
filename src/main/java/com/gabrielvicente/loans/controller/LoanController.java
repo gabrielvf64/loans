9d0e688f7fb76a2a834f9cb57232c59907d033bd
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-12 10:02:56 -0300

    feat: start checkPossibleLoans impl

---
+++ src/main/java/com/gabrielvicente/loans/controller/LoanController.java
@@ -0,0 +1,25 @@
+package com.gabrielvicente.loans.controller;
+
+import com.gabrielvicente.loans.dto.CustomerLoanRequest;
+import com.gabrielvicente.loans.dto.CustomerLoanResponse;
+import com.gabrielvicente.loans.service.LoanService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    private final LoanService loanService;
+
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
+    @PostMapping(value = "/customer-loans")
+    public ResponseEntity<CustomerLoanResponse> checkPossibleLoans(@RequestBody CustomerLoanRequest customerLoanRequest) {
+        CustomerLoanResponse customerLoanResponse = loanService.checkPossibleLoans(customerLoanRequest);
+        return ResponseEntity.ok(customerLoanResponse);
+    }
+}
---
+++ src/main/java/com/gabrielvicente/loans/service/LoanService.java
@@ -0,0 +1,14 @@
+package com.gabrielvicente.loans.service;
+
+import com.gabrielvicente.loans.dto.CustomerLoanRequest;
+import com.gabrielvicente.loans.dto.CustomerLoanResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanService {
+
+    public CustomerLoanResponse checkPossibleLoans(CustomerLoanRequest customerLoanRequest) {
+        return null;
+
+    }
+}
